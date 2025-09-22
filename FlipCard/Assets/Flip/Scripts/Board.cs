1d2f0bf7081319cd0c1972a6fae2b2314d27a7d3
dhdlqns12 <dhdlqns12@gmail.com>
2025-09-22 20:27:45 +0900

    Feat Instant Card

---
+++ FlipCard/Assets/Flip/Scripts/Board.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Board : MonoBehaviour
+{
+    public GameObject cardPrefab;
+    public Transform board;
+
+    void Start()
+    {
+        for(int i = 0;i<10;i++)
+        {
+            int row = i / 5;
+            int col = i % 5;
+
+            float x = col - 2;
+            float y = row * 1.5f;
+
+            Vector3 pos = new Vector3(x, y, 0);
+            GameObject card = Instantiate(cardPrefab, board);
+            card.transform.localPosition = pos;
+            card.name = $"Card_{i}";
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
