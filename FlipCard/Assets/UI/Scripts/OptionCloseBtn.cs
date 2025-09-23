8d010b6443b7987f6b63cd43f833a2eef90d2935
s3ungy3on <s3ungy3on@gmail.com>
2025-09-23 12:26:09 +0900

    panel

--- FlipCard/Assets/UI/Scripts/OptionCloseBtn.cs
+++ FlipCard/Assets/UI/Scripts/OptionCloseBtn.cs
@@ -6,6 +6,7 @@ public class OptionCloseBtn : MonoBehaviour
 {
     public GameObject optionPanel;
 
+    //클릭 시 옵션 패널창 닫기
     public void OnCloseButtonClick()
     {
         optionPanel.SetActive(false);
--- FlipCard/Assets/UI/Scripts/OptionOnBtn.cs
+++ FlipCard/Assets/UI/Scripts/OptionOnBtn.cs
@@ -7,6 +7,7 @@ public class OptionOnBtn : MonoBehaviour
 {
     public GameObject optionPanel;
 
+    //클릭 시 옵션창 활성화
     public void OnButtonClick()
     {
         optionPanel.SetActive(!optionPanel.activeSelf);
