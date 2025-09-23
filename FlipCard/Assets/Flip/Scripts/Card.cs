4f3c8a42bd92edaefafb44eb57ddc897bbd0b5a5
dldygksp9701-wq <dldygksp9701@gmail.com>
2025-09-23 11:40:15 +0900

    Revert "animation"
    This reverts commit 48d361f1494e286b8001e668f9a1d9d46a0ba0a1.

--- FlipCard/Assets/Flip/Scripts/Card.cs
+++ FlipCard/Assets/Flip/Scripts/Card.cs
@@ -32,7 +32,7 @@ public class Card : MonoBehaviour
     //카드 열었을떄
     public void OpenCard()
     {
-        anim.SetBool("isopen", true);
+        anim.SetBool("isOpen", true);
         front.SetActive(true);
         back.SetActive(false);
 
