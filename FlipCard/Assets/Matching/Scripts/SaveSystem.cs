06556662415fe5b4545bb963c009371bdc3774b0
kdk7992-sketch <kdk7992@gmail.com>
2025-09-25 16:12:21 +0900

    StageManager

--- FlipCard/Assets/Matching/Scripts/SaveSystem.cs
+++ FlipCard/Assets/Matching/Scripts/SaveSystem.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class SaveSystem : MonoBehaviour
 {
-    public static void SaveHihestStage(int highestStage)
+    public static void SaveHighestStage(int highestStage)
     //스테이지 해금 상태를 저장(최고 해금 스테이지로 번호를 저장)
     {
-        PlayerPrefs.SetInt("Highest Stage", highestStage);
+        PlayerPrefs.SetInt("HighestStage", highestStage);
         //가장 높은 최고 스테에지로 저장 stage=키 스테이지 값 기록.
         PlayerPrefs.Save();
         //plyerprefs 저장 기록
     }
-    public static int LoadStage()
+    public static int LoadHighestStage()
     {
         return PlayerPrefs.GetInt("HighestStage", 1);
         //HighestStage로 저장된 값 불러오고,게임을 처음 시작하면 기본 최고 스테이지는 1
---
+++ FlipCard/Assets/UI/Scripts/StageManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public static StageManager Instance;
+
+    [Header("스테이지 오브젝트")]
+    public GameObject[] stageObjects;      // 1~4 스테이지 GameObject
+
+    [Header("스테이지 해금 상태")]
+    public int highestStage = 1;           // 현재 해금된 최고 스테이지
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        highestStage = SaveSystem.LoadHighestStage();
+        UpdateStageUI();
+    }
+
+    // UI 갱신 - GameObject 활성/비활성
+    public void UpdateStageUI()
+    {
+        for (int i = 0; i < stageObjects.Length; i++)
+        {
+            stageObjects[i].SetActive(i < highestStage);
+        }
+    }
+
+    // 스테이지 클리어 시 다음 스테이지 해금
+    public void UnlockNextStage(int clearedStage)
+    {
+        if (clearedStage >= highestStage)  //클리어 스테이지 해금 
+        {
+            highestStage = clearedStage + 1;  // 최종 스테이지는 클리어한 스테이지 보다 +1 임
+            if (highestStage > stageObjects.Length)
+                highestStage = stageObjects.Length;
+
+            UpdateStageUI();
+            SaveSystem.SaveHighestStage(highestStage);
+        }
+    }
+}
+
+
+
